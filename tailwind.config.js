d50a285462ffc75e0c7d8d6208ef4af51c57e98d
apkbusinessflr-stack <apkbusinessflr@gmail.com>
2025-10-13 21:26:37 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,6 @@
-module.exports = { plugins: { tailwindcss: {}, autoprefixer: {} } };
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./pages/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: { extend: {} },
+  plugins: []
+};
