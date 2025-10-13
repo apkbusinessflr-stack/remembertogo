388c38f19ca8f41ff819f3db9a49d37bc00f8b36
apkbusinessflr-stack <apkbusinessflr@gmail.com>
2025-10-13 21:28:06 +0300

    Update page.tsx

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,9 +1,6 @@
 "use client";
-
 import dynamic from "next/dynamic";
-
 const Map = dynamic(() => import("@/components/Map"), { ssr: false });
-
 export default function MapPage() {
   return (
     <main className="space-y-4">
